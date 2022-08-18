49a577ae536647f74dbdc34ccf5f60dfedc8987e
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2022-08-18 16:34:39 -0700

    Add timing measurements

--- main.go
+++ main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
+
 	"github.com/yorandrei/vhdl_libs_lister/libs"
 )
 
 func main() {
+	start := time.Now()
 	files, err := libs.ListFiles()
+	t := time.Now()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	fmt.Println("ListFiles took: ", t.Sub(start))
 
 	/*
 		fmt.Println("Inspected Source Files:")
@@ -21,9 +26,12 @@ func main() {
 		fmt.Println("Found Libraries:")
 	*/
 
+	start = time.Now()
 	libraries := libs.GetLibraries(files)
+	t = time.Now()
 	for _, l := range libraries {
 		fmt.Println(l)
 	}
+	fmt.Println("Searching libraries took: ", t.Sub(start))
 
 }
